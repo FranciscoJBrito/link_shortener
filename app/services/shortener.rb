17f2b3b3ce2d7002a4a5e4934708d4c3b617a094
Francisco Brito <franciscojavierbrito16@gmail.com>
2024-01-29 22:53:06 -0300

    refactoring

--- app/services/shortener.rb
+++ app/services/shortener.rb
@@ -11,22 +11,16 @@ class Shortener
       end
 
       def lookup_code
-            # 1. get a lookup code
-            # 2. check if it already exists
-            # 3. break out of the loop if it does not exist
-            # 4. if not, get another code
-            i = 0
             loop do
-                  code = get_fresh_code(i)
+                  code = get_fresh_code
                   break code unless link_model.exists?(lookup_code: code)
-                  i = i + 1
             end
       end
 
       private
 
-      def get_fresh_code(i)
-            Digest::SHA256.hexdigest(url)[i..(i + 6)]
+      def get_fresh_code
+            SecureRandom.uuid[0..6]
       end
       
       
