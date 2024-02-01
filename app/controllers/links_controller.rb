c12b5e7d006103dd3bb0313cd58e11c02b6e0c88
Francisco Brito <franciscojavierbrito16@gmail.com>
2024-02-01 20:26:21 -0300

    adding yarn dependencies

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -3,6 +3,11 @@ class LinksController < ApplicationController
       def create
             shortener = Shortener.new(link_params[:original_url])
             @link = shortener.generate_short_link
+
+            respond_to do |format|
+                  format.html { redirect_to root_path }
+                  format.js   # Esto buscará el archivo create.js.erb en app/views/links/
+            end
       end
 
       private
