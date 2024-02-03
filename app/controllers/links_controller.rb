d8f0b7e3cf818ee6ad0701bca7a5a9575cf5c279
Francisco Brito <franciscojavierbrito16@gmail.com>
2024-02-02 21:58:10 -0300

    adding  pry gem for test and development env

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -3,11 +3,11 @@ class LinksController < ApplicationController
       def create
             shortener = Shortener.new(link_params[:original_url])
             @link = shortener.generate_short_link
-
             respond_to do |format|
                   format.html { redirect_to root_path }
-                  format.js  { render :create } # Esto buscará el archivo create.js.erb en app/views/links/
+                  format.js 
             end
+            #binding.pry
       end
 
       private
