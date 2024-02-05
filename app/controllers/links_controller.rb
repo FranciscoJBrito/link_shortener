e9df259d7f59c5f4e1cbfa92efc95440a5dc360d
Francisco Brito <franciscojavierbrito16@gmail.com>
2024-02-04 23:46:04 -0300

    resolving compatibility issues

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -3,11 +3,10 @@ class LinksController < ApplicationController
       def create
             shortener = Shortener.new(link_params[:original_url])
             @link = shortener.generate_short_link
-            respond_to do |format|
-                  format.html { redirect_to root_path }
-                  format.js 
-            end
-            #binding.pry
+            #respond_to do |format|
+            #      format.html { redirect_to root_path }
+            #      format.js { render js: "alert('Error al guardar el enlace');"  }
+            #end
       end
 
       private
