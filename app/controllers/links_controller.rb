7477036b6c892dba05426116744f7a53246f1e24
Francisco Brito <franciscojavierbrito16@gmail.com>
2024-02-08 15:03:59 -0300

    solving javascript execution problems

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -3,10 +3,10 @@ class LinksController < ApplicationController
       def create
             shortener = Shortener.new(link_params[:original_url])
             @link = shortener.generate_short_link
-            #respond_to do |format|
-            #      format.html { redirect_to root_path }
-            #      format.js { render js: "alert('Error al guardar el enlace');"  }
-            #end
+            respond_to do |format|
+                  format.html { redirect_to root_path }
+                  format.js
+            end
       end
 
       private
@@ -14,6 +14,6 @@ class LinksController < ApplicationController
       def link_params
             params.require(:link).permit(:original_url)
       end
-      
-      
+
+
 end
