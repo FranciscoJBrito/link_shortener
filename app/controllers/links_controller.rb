c706b20d3d71392e8622fc5c1a781f6becc49cd7
Francisco Brito <franciscojavierbrito16@gmail.com>
2024-02-09 13:13:55 -0300

    resolving compatibility between gems

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -3,10 +3,24 @@ class LinksController < ApplicationController
       def create
             shortener = Shortener.new(link_params[:original_url])
             @link = shortener.generate_short_link
-            #respond_to do |format|
-            #      format.html { redirect_to root_path }
-            #      format.js
+
+            respond_to do |format|
+                  if @link.persisted?
+                    format.js { render 'create.js.erb' }
+                  else
+                    format.js { render 'error.js', status: :unprocessable_entity }
+                  end
+            end
+
+            #if @link.persisted?
+            #      respond_to do |format|
+            #           format.js
+            #            format.html { redirect_to root_path }
+            #      end
+            #else
+            #      render 'home/index'
             #end
+
       end
 
       private
